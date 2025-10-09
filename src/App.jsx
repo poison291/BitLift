7d574f7ca9d595be132faec7d94879917e0e2143
poison291 <susand.dev291@gmail.com>
2025-10-09 19:08:12 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  
+  return (
+    <>
+    <div>
+     <h1 className="text-red-500">P2P Connection</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
